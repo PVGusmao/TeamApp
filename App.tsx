180950090ca763bacbadedbd96e6d762fef056dc
Paulo Gusmao <paulo.gusmao@grupoaguia.corp>
2022-10-21 14:27:27 -0300

    implementado as rotas da aplicação

--- App.tsx
+++ App.tsx
@@ -1,11 +1,12 @@
+import { StatusBar } from 'react-native';
 import { ThemeProvider } from 'styled-components/native';
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
 
+import { Loading } from './src/components/Loading';
+
 import theme from './src/theme';
 
-import { Players } from './src/screens/Players';
-import { Loading } from './src/components/Loading';
-import { StatusBar } from 'react-native';
+import { Routes } from './src/routes'; 
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
@@ -18,7 +19,7 @@ export default function App() {
       translucent
     />
       {
-        fontsLoaded ? <Players /> : <Loading />
+        fontsLoaded ? <Routes /> : <Loading />
       }
     </ThemeProvider>
   );
